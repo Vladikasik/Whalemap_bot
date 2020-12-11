209aaf9765d37c1df8a2245059af3d5b39479277
Vlad <vsakayafignavlad@gmail.com>
2020-12-11 15:54:27 +0300

    Main menu only

---
+++ msg.py
@@ -0,0 +1,5 @@
+greeting = 'What notification do you want to receive?What notification do you want to receive?'
+choose_main = ['🐋Whale inflows',
+               '🦜Sopr',
+               '👴Hodler volumes',
+               '⛰Large transactions']
