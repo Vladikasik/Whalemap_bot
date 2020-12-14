f01b60e0d98ea84c660c56797dd210facd848a1f
Vlad <vsakayafignavlad@gmail.com>
2020-12-14 10:24:28 +0300

    DB working

--- database_config.py
+++ database_config.py
@@ -12,7 +12,6 @@ class DB:
         if group:
             with open(self.groups_file, "r") as file:
                 groups_data = json.load(file)
-            groups_data[data[1]][data[2]].append(data[0])
             if data[0] not in groups_data[data[1]][data[2]]:
                 groups_data[data[1]][data[2]].append(data[0])
                 with open(self.groups_file, 'w') as file:
@@ -20,14 +19,21 @@ class DB:
                 print('groups updated')
             else:
                 print('user already subsribed to this plan')
+
         elif user:
             with open(self.users_file, "r") as file:
                 users_data = json.load(file)
+            data[0] = str(data[0])
             try:
-                if data[1] not in users_data[data[0]]:
+                if str(data[1]) not in users_data[data[0]]:
                     users_data[data[0]].append(data[1])
+                else:
+                    print('user already exists')
             except KeyError:
-                users_data[data[0]] = [data[1]]
+                users_data[int(data[0])] = [data[1]]
+            with open(self.users_file, 'w') as file:
+                json.dump(users_data, file)
+            print('users updated')
 
     def get_user(self, user_id):
         with open(self.users_file, "r") as file:
